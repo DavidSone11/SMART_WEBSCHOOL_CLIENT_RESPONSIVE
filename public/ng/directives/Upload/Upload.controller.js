73f2b70a9c8989f6a769c6efaf148b351f3432ab
david <david11sone@gmail.com>
2018-05-16 02:05:47 +0530

    dasdsa

--- public/ng/directives/Upload/Upload.controller.js
+++ public/ng/directives/Upload/Upload.controller.js
@@ -3,10 +3,10 @@ var app = angular.module("sbAdminApp");
 app.controller("UploadController", ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
 
     $scope.files = [];
-    $scope.user ={
-        data:''
+    $scope.user = {
+        data: ''
     }
-    
+
     $scope.getUserUploads = function (query, timeout) {
         return $http.get('http://localhost:4000/api/v1/userUpload/findByfilename?name=' + query);
 
@@ -18,9 +18,22 @@ app.controller("UploadController", ['$scope', '$timeout', '$http', function ($sc
 
             $scope.user.data = selected.originalObject.img.data["data"];
             $scope.contentType = selected.originalObject.img.contentType;
-            $scope.base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(selected.originalObject.img.data["data"])));
+            // $scope.base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(selected.originalObject.img.data["data"])));
+            // $scope.base64String = btoa(selected.originalObject.img.data["data"]);
+            $scope.base64String = _arrayBufferToBase64(selected.originalObject.img.data["data"]);
+
         }
     };
+
+    function _arrayBufferToBase64(buffer) {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
 
 
 }]);
