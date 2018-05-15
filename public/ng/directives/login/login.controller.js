ab4622bf9bd3e4f5a1c029f1d67302496a88e70e
david <david11sone@gmail.com>
2018-05-15 17:15:59 +0530

    dasdasd

--- public/ng/directives/login/login.controller.js
+++ public/ng/directives/login/login.controller.js
@@ -2,7 +2,8 @@
 var app = angular.module("sbAdminApp");
 app.controller("LoginController", function ($scope, $state, $window) {
 
-
+    $scope.isRegister = false;
+    $scope.isForget =false;
 
     $scope.login = function (username, password) {
 
