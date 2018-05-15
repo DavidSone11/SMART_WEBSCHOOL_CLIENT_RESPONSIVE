058710fcf1ca5e5ebb07ce55d51a100698dc52fc
david <david11sone@gmail.com>
2018-05-16 01:35:42 +0530

    klkl

--- public/ng/directives/Upload/Upload.controller.js
+++ public/ng/directives/Upload/Upload.controller.js
@@ -1,7 +1,26 @@
 
 var app = angular.module("sbAdminApp");
-app.controller("UploadController",  ['$scope','$timeout','$http',function($scope,$timeout,$http) {
-   
+app.controller("UploadController", ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
+
     $scope.files = [];
+    $scope.user ={
+        data:''
+    }
+    
+    $scope.getUserUploads = function (query, timeout) {
+        return $http.get('http://localhost:4000/api/v1/userUpload/findByfilename?name=' + query);
+
+    }
+
+
+    $scope.SelectedImage = function (selected) {
+        if (selected) {
+
+            $scope.user.data = selected.originalObject.img.data["data"];
+            $scope.contentType = selected.originalObject.img.contentType;
+            $scope.base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(selected.originalObject.img.data["data"])));
+        }
+    };
+
 
 }]);
