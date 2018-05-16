d0387836310100bce78887e3a6019b61d5e0b65a
david <david11sone@gmail.com>
2018-05-17 01:29:09 +0530

    dasasd

--- public/ng/directives/r_sidebar/r_sidebar.directive.js
+++ public/ng/directives/r_sidebar/r_sidebar.directive.js
@@ -8,11 +8,34 @@ app.directive("rsidebar", ['$compile', function ($compile) {
 
         controller: function ($scope) {
 
+            $scope.isToggle = true;
+
             $scope.rightToggler = function () {
 
-                $(function () {
-                  $('#rightSidebar').animate({width: "30px"},"slow");
-                });
+                if ($scope.isToggle) {
+                    $(function () {
+                        $('#rightSidebar').animate({ width: "0px" }, "slow");
+                        $('#rightoggleBtn').animate({ left: "1300px" }, "slow");
+                        // $("#main-page-wrapper").css("background-color", 'black');
+                        // $("#main-page-wrapper").css("opacity", 0.4);
+                        $('body').css("background-color", 'black');
+                        $("body").css("opacity", 0.2);
+
+                    });
+
+                } else {
+
+                    $(function () {
+                        $('#rightSidebar').animate({ width: "83%" }, "slow");
+                        $('#rightoggleBtn').animate({ left: "178px" }, "slow");
+                        // $('body').css("background-color", 'black');
+                        $("body").css("opacity",1);
+                    });
+
+                }
+
+                $scope.isToggle = !$scope.isToggle;
+
 
             }
 
