47e881154d48447ecd1ce570b408c7ae0bd475c9
david <david11sone@gmail.com>
2018-05-16 15:34:06 +0530

    dasda

--- public/ng/directives/User/User.directive.js
+++ public/ng/directives/User/User.directive.js
@@ -4,10 +4,20 @@ app.directive("user", ['$compile', function ($compile) {
     return {
         restrict: 'E',
         replace: true,
-        templateUrl: 'ng/directives/User/User.tmpl.html',
-        link: function (scope, elem, attr) {
+        templateUrl: 'ng/directives/User/User.directive.html',
+        compile: function (elem, attrs) {
+            // elem.css("border-bottom", "1px solid grey");
+            // elem.css("border-top", "1px solid grey");
+            elem.css("margin", "0 0 0 0");
 
+            var link = function (scope, elem, attrs) {
+
+                // elem.html("User First Name: <b>" + 'santosh' + "</b> , User Last Name: <b>" + 'sahu' + "</b>" + ", User Age: <b>" + '21' + "</b>");
+                // elem.css("background-color", "yellow");
+            }
+            return link;
         },
+
         controller: function ($scope) {
 
         }
